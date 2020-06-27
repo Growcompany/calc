b7d9adba37fc9229dba6907ac215cf9dd678860e
Growcompany <66565360+Growcompany@users.noreply.github.com>
2020-06-28 04:29:26 +0900

    Update template.py

--- template.py
+++ template.py
@@ -2,12 +2,12 @@ html = b"""
 <html>
     <body>
         <form action="">
-            y = <input type="number" name="a"> * x^2
-              + <input type="number" name="b"> * x
-              + <input type="number" name="c"><br><br>
+	a = <input type = "number" name = "a"> <br><br>
+	b = <input type = "number" name = "b">
             <input type="submit">
         </form>
-        <img src="/graph.png" alt="Result image here">
+         Sum = %(sum)d <br><br>
+         Mul = %(mul)d
     </body>
 </html>
 """
